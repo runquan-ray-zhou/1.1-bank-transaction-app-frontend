ca1df65a2e6ec8e22248fc0289e25b57074a4340
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-04 23:55:12 -0400

    display transaction date and amount and add css

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom"
+import "./Transaction.css"
 
 export default function Transaction({ transaction }) {
     return (
-        <div>
+        <div className="Transaction">
+            <p>{transaction.date}</p>
             <Link to={`/transactions/${transaction.id}`}>{transaction.item_name}</Link>
+            <p>{transaction.amount}</p>
         </div>
     )
 }
