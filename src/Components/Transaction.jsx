9f640b238e141e10851650ff588c3d2ecfe9ebdf
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-05 21:36:40 -0400

    add css to transaction and add delete button

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -1,12 +1,40 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "./Transaction.css"
 
+const API = import.meta.env.VITE_API_URL
+
 export default function Transaction({ transaction }) {
+
+    let navigate = useNavigate()
+
+    function handleDelete() {
+        fetch(`${API}/transactions/${transaction.id}`, {
+            method: "DELETE"
+        })
+        .then(() => navigate("/transactions"))
+        .catch((error) => console.error(error))
+    }
+
     return (
-        <div className="Transaction">
+        <tr className="Transaction">
+            <td>
             <p>{transaction.date}</p>
+            </td>
+            <td>
             <Link to={`/transactions/${transaction.id}`}>{transaction.item_name}</Link>
+            </td>
+            <td>
+            <p>{transaction.from}</p>
+            </td>
+            <td>
+            <p>{transaction.category}</p>
+            </td>
+            <td>
             <p>{transaction.amount}</p>
-        </div>
+            </td>
+            <td>
+            <button onClick={ handleDelete }>Delete</button>
+            </td>
+        </tr>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div className="Home">
-        <h1>Welcome to My Bank Transaction App!</h1>
+        <h1>Welcome to Bank Bank App!</h1>
       </div>
     );
   }
