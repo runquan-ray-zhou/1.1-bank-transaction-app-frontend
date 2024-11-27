95dbfeae12020b599cb3bf746dde49800f59c7c0
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-11-26 23:56:37 -0500

    change login to enter

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,29 +1,28 @@
 import { Link } from "react-router-dom";
-import "./Login.css"
-import bankIcon from "../assets/bank-svgrepo-com1.svg"
+import "./Login.css";
+import bankIcon from "../assets/bank-svgrepo-com1.svg";
 
 export default function Login() {
-    
-    return (
-        <div className="login">
-            <div className="circle">
-                <div className="circle1">
-                    <div className="circle2">
-                        <div className="circle3">
-                        <img src={bankIcon} alt="bank icon" />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="login">
+      <div className="circle">
+        <div className="circle1">
+          <div className="circle2">
+            <div className="circle3">
+              <img src={bankIcon} alt="bank icon" />
             </div>
-            <span>Bank Bankster</span>
-            <div className="login-button__container">
-                <div className="edition">
-                <p>2024 EDITION</p>
-                </div>
-                <Link to="/home">
-                    <button className="login-button">LOGIN</button>
-                </Link>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+      <span>Bank Bankster</span>
+      <div className="login-button__container">
+        <div className="edition">
+          <p>2024 EDITION</p>
+        </div>
+        <Link to="/home">
+          <button className="login-button">ENTER</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
